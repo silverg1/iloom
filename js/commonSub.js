654e15aa741f5a91fd3b8fddc3ae5dd3bdde7ee9
silverg1 <37791485+silverg1@users.noreply.github.com>
2018-06-06 22:59:51 +0900

    반응형 gnb 구현

--- js/commonSub.js
+++ js/commonSub.js
@@ -4,5 +4,7 @@ $(function(){
   $('#move').load('main.html #moveHeader');
   $('#allm').load('main.html #allMenuPopup');
   $('#footer').load('main.html footer');
+  $('#bnd').load('main.html #blind');
+  $('#gm').load('main.html #gnbM');
 
 });
