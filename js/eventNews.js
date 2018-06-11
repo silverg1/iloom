4c6f5c9bc87cbbd39a52df6aa516d48a1a83b13f
silverg1 <37791485+silverg1@users.noreply.github.com>
2018-06-12 02:28:24 +0900

    .
    회원가입~ing

--- js/eventNews.js
+++ js/eventNews.js
@@ -1,5 +1,6 @@
 $(function(){
 
+// event & news
   $('#evBtn2').click(function(){
     $('.evList.show').hide();
     $('.evList.hide').show();
@@ -13,5 +14,35 @@ $(function(){
     $(this).addClass('choose');
     $('#evBtn2').removeClass('choose');
   });
+
+
+// store
+  $('.comeBtn').click(function(){
+    $(this).parent().parent().siblings('.come').slideToggle(300);
+  });
+  $('.moreBtn button').click(function(){
+    $(this).hide();
+    $('.more').show();
+  });
+
+
+
+// login
+
+$('.log_chk img, .log_chk p').click(function(){
+  $('.log_chk img').toggleClass('chk');
+});
+$('.nonMember').click(function(){
+  $('.mem').hide();
+  $('.nonmem').show();
+  $(this).addClass('memchk');
+  $('.member').removeClass('memchk');
+});
+$('.member').click(function(){
+  $('.nonmem').hide();
+  $('.mem').show();
+  $(this).addClass('memchk');
+  $('.nonMember').removeClass('memchk');
+});
 
 });
